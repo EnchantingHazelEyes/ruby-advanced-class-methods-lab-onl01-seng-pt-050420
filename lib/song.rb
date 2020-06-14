df82033cde46220ac7465e02946f7517f9929b63
Samantha Barros <samantha.barros95@gmail.com>
2020-06-14 14:37:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,64 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    s = self.new 
+    s.save
+    s
+  end 
+  
+  def self.new_by_name(string_name_of_song)
+    s = self.new 
+    s.name = string_name_of_song
+    s
+  end 
+  
+   def self.create_by_name(string_name_of_song)
+    s = self.new 
+    s.name = string_name_of_song
+    s.save
+    s 
+  end 
+  
+  def self.find_by_name(string_name_of_song)
+    self.all.detect{|n| n.name == string_name_of_song}
+  end 
+  
+    def self.find_or_create_by_name(find_this_song)
+      did_i_find_it = self.all.detect {|n| n.name == find_this_song}
+      if did_i_find_it == nil
+        s = self.new 
+        s.name = find_this_song
+        s.save 
+        s
+      else
+        did_i_find_it
+      end
+  end 
+  
+  def self.alphabetical
+    self.all.sort_by {|x| x.name}
+  end 
+  
+  def self.new_from_filename(mp3_formatted_file)
+     s = self.new 
+    s.name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[1] 
+    s.artist_name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[0]
+    s
+  end 
+  
+  def self.create_from_filename(mp3_formatted_file)
+     s = self.new 
+    s.name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[1] 
+    s.artist_name = mp3_formatted_file.split(/[^a-zA-Z\s]|\s-\s/)[0]
+    s.save
+    s
+  end 
+  
+  def self.destroy_all
+    self.all.clear
+  end 
+  
+  
 end
